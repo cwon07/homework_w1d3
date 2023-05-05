30d8ee26b5e514f2cff136d1983cc2a268fd558f
Christine Wong <jumiwong@Christines-MacBook-Air.local>
2023-05-04 23:17:33 -0700

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -18,7 +18,7 @@
 // Fizz Buzz
 ////////////////////////////////
 
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)){
         console.log("FizzBuzz")}
 
@@ -31,12 +31,22 @@ for (let i = 1; i <= 100; i++) {
 else {
     console.log(i)
 }
-}
+}*/
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee.splice(2, 2, 5001) // this doesn't print Morder though? check before submitting
+wolfy.splice(3,3,"Gatham City")
+dart.push("Hawkins")
+wolfy.splice(wolfy.indexOf("Wolfy"), 1, "Gameboy")
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
