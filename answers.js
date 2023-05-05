57dea16d2b58f9302c5aaa4f454f7200860ca4c9
Christine Wong <jumiwong@Christines-MacBook-Air.local>
2023-05-04 23:18:58 -0700

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -37,25 +37,25 @@ else {
 // Wild Wild Life
 ////////////////////////////////
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-plantee.splice(2, 2, 5001) // this doesn't print Morder though? check before submitting
-wolfy.splice(3,3,"Gatham City")
-dart.push("Hawkins")
-wolfy.splice(wolfy.indexOf("Wolfy"), 1, "Gameboy")
+// plantee.splice(2, 2, 5001) // this doesn't print Morder though? check before submitting
+// wolfy.splice(3,3,"Gatham City")
+// dart.push("Hawkins")
+// wolfy.splice(wolfy.indexOf("Wolfy"), 1, "Gameboy")
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
 
-/*const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
-for (let upperName of ninjaTurtles) {
-    console.log(upperName.toUpperCase());
-}*/
+// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+// for (let upperName of ninjaTurtles) {
+//     console.log(upperName.toUpperCase());
+// }
 
 ////////////////////////////////
 // Methods, Revisited
