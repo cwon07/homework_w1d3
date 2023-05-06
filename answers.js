0cade108c631df53649ec98692251d1bad66e6e4
Christine Wong <jumiwong@Christines-MacBook-Air.local>
2023-05-06 06:42:53 -0700

    Find the Median answered

--- answers.js
+++ answers.js
@@ -104,3 +104,16 @@ console.log(whereIsWaldo)
 //         console.log(`${i} Love me, pet me! HSSSSSS!`)
 //     }
 // }
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
+
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+// nums.sort((a , b) => a - b)
+// let median;
+// if(nums.length % 2 != 0) {
+//     let middleIndex = Math.floor(nums.length/2)
+//     median = nums[middleIndex]
+// } 
+// console.log(median)
